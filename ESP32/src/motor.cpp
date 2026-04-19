e13c2e6d8115921cc8b9e11759cb2163359f1d86
Gabriel Pardo <136826784+GabrieIPardo@users.noreply.github.com>
2026-04-19 20:48:56 -0300

    Add Debounce

--- ESP32/src/motor.cpp
+++ ESP32/src/motor.cpp
@@ -1,79 +1,114 @@
 /**************** Definição de Pinos***************/
-#define PinoMotorSobe 6
-#define PinoMotorDesce 3
+#define PinoMotorDesce 6  //ESP pino 25
+#define PinoMotorSobe 5  //Esp pino 26
 
-#define PinoBtnEmergencia 8
-#define PinoBtnPausa 0
-#define PinoBtnAcionamento 0
+#define PinoBtnEmergencia 8  //Esp pino 32
+#define PinoBtnPausa 9  //Esp pino 33
+#define PinoBtnAcionamento 10  //Esp pino 34
 
 
 /*********************Variáveis********************/
+char* listaEstadosPrensa[] = {"esperando", "emergencia", "descendo", "subindo", "pausando"};
+
 enum EstadosPrensa{
-  	espera = 0,
+  	esperando = 0,
     emergencia,
 	descendo,
   	subindo,
-    pausa //termina a ação e entra em estado de pausa
+    pausando //termina a ação e entra em estado de pausa
 };
 
-EstadosPrensa estadoPrensa = descendo;
+EstadosPrensa estadoPrensa = esperando;
+EstadosPrensa estadoAnteriorPrensa = esperando;
+
+int passoPwm = 5; //define o salto da função softStart()
+int passoPwmEmergencia = 254; //define o salto da função softStop() em estado de Emergência
+int pwmMaximo = 255; // define o PWM máximo usado no softStart() e softStop()
 
 int estadoPwm = 0;
+
 bool esperandoPausa = 0;
 
+bool estadoBtnAcionamento = 0;
+bool estadoBtnEmergencia = 0;
+bool estadoBtnPausa = 0;
+
+bool estadoAnteriorEmergencia = 0;
+bool estadoAnteriorPausa = 0;
+
+unsigned long tempoUltimaEmergencia = millis();
+unsigned long tempoUltimaPausa = millis();
+int tempoDebounce = 200;
+
+
+
+/**************************PROTÓTIPO**********************/
+void estadoSerial(EstadosPrensa estadoAtual);
+
+
 /**************************SETUP**************************/
-void setup()
-{
+void setup(){
   Serial.begin(9600);
   pinMode(PinoMotorDesce, OUTPUT);
   pinMode(PinoMotorSobe, OUTPUT);
-  pinMode(PinoBtnEmergencia, INPUT);
+  pinMode(PinoBtnEmergencia, INPUT_PULLUP);
+  pinMode(PinoBtnPausa, INPUT_PULLUP);
+  pinMode(PinoBtnAcionamento, INPUT_PULLUP);
+
 }
 
-void loop()
-{
+void loop() {
     switch(estadoPrensa){
-        case espera:
-      		descer();
-            break;
+        case esperando:
+      	  estadoSerial(estadoPrensa);
+          esperar();
+          break;
         
         case emergencia:
-            alertar();
-            break;
+      	  estadoSerial(estadoPrensa);
+          alertar();
+          break;
         
         case descendo:
-      		descer();
-            break;
+      	  estadoSerial(estadoPrensa);
+      	  descer();
+          break;
         
         case subindo:
-            subir();
-            break;
+      	  estadoSerial(estadoPrensa);
+          subir();
+          break;
 
-        
-        case pausa:
-            pausar();
-            break;
+        case pausando:
+      	  estadoSerial(estadoPrensa);
+          pausar();
+          break;
     }
 }
 
 
 /****************Funções**********************/
 void esperar(){
+
     verificarEstado();
 
-    if(esperandoPausa == 1){
-        estadoPrensa = pausa;
+    if(esperandoPausa){
+        esperandoPausa = 0;
+        estadoAnteriorPrensa = estadoPrensa;
+        estadoPrensa = pausando;
         return;
-    } else {
-        if(estadoPrensa == espera){
-            //Caso Acionamento da Prensa
-            int estadoBtnAcionamento = digitalRead(PinoBtnAcionamento);
-            if(estadoBtnAcionamento){
-                estadoPrensa = descendo;
-                Serial.println("Estado Prensa: Descendo");
-                return;
-            } 
-        }
+    }
+
+    if(estadoPrensa == esperando){
+        //Caso Acionamento da Prensa
+        estadoBtnAcionamento = !digitalRead(PinoBtnAcionamento);
+        
+        verificarEstado();
+        if(estadoBtnAcionamento && !esperandoPausa){
+            estadoAnteriorPrensa = estadoPrensa;
+            estadoPrensa = descendo;
+            return;
+        } 
     }
 
     //lendo sensores
@@ -81,64 +116,174 @@ void esperar(){
 }
 
 void descer(){
-  verificarEstado();
-  if(estadoPwm == 0) {
-  	estadoPwm = pwm();
-  } else {
-  	
-  }
+    verificarEstado();
 
-    //Função verificar corrente atual que motor está puxando
-
-}
-
-void subir(){}
-
-void alertar(){
-    int estadoBtnEmergencia = digitalRead(PinoBtnEmergencia);
-
-    digitalWrite(PinoMotorDesce, LOW);
-    digitalWrite(PinoMotorSobe, LOW);
-
-    if(estadoBtnEmergencia) {
-        subir();
-        return;
-    }
-}
-
-void pausar(){
-
-}
-
-void verificarEstado() {
-    if(esperandoPausa == 1) return;
-    
-    //Caso Pausa
-    int estadoBtnPausa = digitalRead(PinoBtnPausa);
-    if(estadoBtnPausa){
-        esperandoPausa = 1;
-        return;
+    if(estadoPwm == 0){
+        SoftStart();
+    } else {
+      	delay(4000);
+    	SoftStop();
+        estadoAnteriorPrensa = estadoPrensa;
+        estadoPrensa = subindo;
     }
 
-    //Caso Emergência/Alerta
-    int estadoBtnEmergencia = digitalRead(PinoBtnEmergencia);
-    if(estadoBtnEmergencia){
-        estadoPrensa = emergencia;
-        Serial.println("Estado Prensa: Emergencia/Alerta");
+    /*  
+    if(chegar no fundo || ou puxar corrente demais){ 
+        estadoPrensa = subindo;
         return;
-	} 
+    }
+    */
 }
 
-int pwm(){
-	for(int pwm = 0; pwm <= 255; pwm += 4){
-     	if (estadoPrensa == descendo){
-          verificarEstado();
-          digitalWrite(PinoMotorDesce, LOW);
-          analogWrite(PinoMotorSobe, pwm);
-          Serial.println(pwm);
-          delay(20);
-    	}
-  	}
+void subir(){
+    if(estadoPwm == 0){
+        SoftStart();
+    } else {
+        delay(4000);
+        SoftStop();
+        estadoAnteriorPrensa = estadoPrensa;
+      	estadoPrensa = esperando;
+      	return;
+    }
+
+    /*  
+    if(chegar no fundo || ou puxar corrente demais){ 
+        estadoPrensa = espera;
+        return;
+    }
+    */
   
-  	return 255;
+}
+
+//emergencia
+void alertar(){
+    estadoPwm = 0;
+    verificarEstado();
+}
+
+void pausar(){  
+    verificarEstado();
+}
+
+void verificarEstado() {   
+    estadoBtnEmergencia = !digitalRead(PinoBtnEmergencia);
+    estadoBtnPausa = !digitalRead(PinoBtnPausa);
+
+    
+    if(estadoBtnEmergencia && estadoAnteriorEmergencia == 0  && millis() - tempoUltimaEmergencia >= tempoDebounce) {
+        if(estadoPrensa == emergencia){
+            //Caso esperando sair Emergência/Alerta
+            tempoUltimaEmergencia = millis();
+            estadoAnteriorPrensa = estadoPrensa;
+            estadoPrensa = subindo;
+            estadoAnteriorEmergencia = estadoBtnEmergencia;
+            return;
+        } else {
+            //Caso Emergência/Alerta
+            tempoUltimaEmergencia = millis();
+            estadoAnteriorPrensa = estadoPrensa;
+            estadoPrensa = emergencia;
+            esperandoPausa = 0;
+            estadoAnteriorEmergencia = estadoBtnEmergencia;
+            return;    
+        }
+    } 
+
+    if(estadoBtnEmergencia == 0 && estadoAnteriorEmergencia){
+        tempoUltimaEmergencia = millis();
+        estadoAnteriorEmergencia = estadoBtnEmergencia;
+    }
+
+    //Caso Pausa
+    if(estadoBtnPausa && estadoPrensa != emergencia && estadoAnteriorPausa == 0 && millis() - tempoUltimaPausa >= tempoDebounce){
+        if(esperandoPausa){
+            //Caso esperando Pausa
+            return;
+        }
+        
+        if(estadoPrensa == pausando){
+            //Caso esperando sair Pausa
+            tempoUltimaPausa = millis();
+            estadoAnteriorPrensa = estadoPrensa;
+            estadoPrensa = esperando;
+            estadoAnteriorPausa = estadoBtnPausa;
+            return;
+        } else {
+            //Caso acione Pausa
+            tempoUltimaPausa = millis();
+            esperandoPausa = 1;
+            estadoAnteriorPausa = estadoBtnPausa;
+            Serial.println("Estado Prensa: Esperando Pausa");
+            return;
+        }
+        
+    }
+    if(estadoBtnPausa == 0){
+        tempoUltimaPausa = millis();
+        estadoAnteriorPausa = estadoBtnPausa;
+    }
+}
+
+void SoftStart(){
+    if(estadoPwm != 0) return;
+
+	for(int pwm = 0; pwm <= pwmMaximo; pwm += passoPwm){ 
+      	verificarEstado();
+
+        if(estadoPrensa == emergencia){
+            analogWrite(PinoMotorDesce, 0);
+            analogWrite(PinoMotorSobe, 0);
+            return;
+        }
+        
+      	if (estadoPrensa == subindo){
+            analogWrite(PinoMotorDesce, 0);
+            analogWrite(PinoMotorSobe, pwm);
+            Serial.println("Subindo: " + String(map(pwm, 0, 255, 0, 100)) + "%");
+            delay(200);
+
+    	} else if(estadoPrensa == descendo) {
+            analogWrite(PinoMotorDesce, pwm);
+            analogWrite(PinoMotorSobe, 0);            
+            Serial.println("Descendo: " + String(map(pwm, 0, 255, 0, 100)) + "%");
+            delay(200);        
+        } 
+  	}
+  	estadoPwm = 255;
+}
+
+void SoftStop(){
+	for(int pwm = pwmMaximo; pwm >= 0; pwm -= passoPwm){ 
+      	verificarEstado();
+
+        if(estadoPrensa == emergencia){
+            analogWrite(PinoMotorDesce, 0);
+            analogWrite(PinoMotorSobe, 0);
+            return;
+        }
+
+      	if (estadoPrensa == subindo){
+            analogWrite(PinoMotorDesce, 0);
+            analogWrite(PinoMotorSobe, pwm);
+            
+            Serial.println("Velocidade: " + String(map(pwm, 0, 255, 0, 100)) + "%");
+            delay(200);
+
+    	} else if(estadoPrensa == descendo) {
+            analogWrite(PinoMotorDesce, pwm);
+            analogWrite(PinoMotorSobe, 0);
+            
+            Serial.println("Velocidade: " + String(map(pwm, 0, 255, 0, 100)) + "%");
+            delay(200);        
+        } 
+  	}
+    estadoPwm = 0;
+    delay(2000);
+}
+
+void estadoSerial(EstadosPrensa estadoAtual){
+    if(estadoAnteriorPrensa != estadoAtual) {
+        Serial.println("Estado Prensa: " + String(listaEstadosPrensa[estadoAtual]));
+        estadoAnteriorPrensa = estadoPrensa;
+    }
 }
